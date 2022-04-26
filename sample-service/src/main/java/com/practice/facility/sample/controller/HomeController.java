d7e0d3671942811a50e222c1da456c013f22534f
leejongwoo <leejongwoo.dev@gmail.com>
2022-04-26 23:13:16 +0900

    Create api gateway and Test sample service routing

---
+++ sample-service/src/main/java/com/practice/facility/sample/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.practice.facility.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sample")
+public class HomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome sample service";
+    }
+
+}
